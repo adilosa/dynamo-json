a4da5f242eadb37f610f83a2928d6edb497c6129
Andrew DiLosa <andrew@dilosa.com>
2020-06-30 16:41:50 -0700

    release 1.2.0

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='dynamo-json',
-    version='1.0.0',
+    version='1.2.0',
     description='Swap between DynamoDB JSON and normal JSON',
     long_description=long_description,
     long_description_content_type='text/markdown',
@@ -23,7 +23,11 @@ setup(
         'Topic :: Software Development :: Libraries :: Python Modules',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
     keywords='dynamo json dynamodb',  # Optional
     py_modules=["dynamo_json", "cli"],
